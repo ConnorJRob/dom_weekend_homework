8d67bf2685ab19620e995a577cac87de090bdecc
Connor <connorjrobertson@hotmail.co.uk>
2021-07-24 15:14:14 +0100

    MVP complete

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const form = document.querySelector('#new-adventurer-form');
     form.addEventListener('submit', handleFormSubmit);
+
+    const deleteAllButton = document.querySelector('#delete_all')
+    deleteAllButton.addEventListener('click', handleDeleteAllClick);
+    
 });
 
 const handleFormSubmit = function (event) {
@@ -10,20 +14,43 @@ const handleFormSubmit = function (event) {
 
     const newAdventurer = document.createElement('li');
     const list = document.querySelector('#adventurer-roster');
+    const adventurer_css_id = "adventurer_" + event.target.adventurer_class.value;
+    newAdventurer.id = adventurer_css_id;
     newAdventurer.className = "adventurer";
     list.appendChild(newAdventurer);
-    
+
     const name = document.createElement('h3');
-    name.textContent = event.target.adventurer_name.value;
+    name.className = "adventurer_name"
+    name.textContent = `Name: ${event.target.adventurer_name.value}`;
     newAdventurer.appendChild(name);
+
+    const adventurer_class_details = document.createElement('div');
+    adventurer_class_details.className = "adventurer_class_details"
+    newAdventurer.appendChild(adventurer_class_details);
 
     const adventurer_class = document.createElement('h4');
     adventurer_class.textContent = event.target.adventurer_class.value;
-    newAdventurer.appendChild(adventurer_class);
+    adventurer_class_details.appendChild(adventurer_class);
+
+    const image = document.createElement('img');
+    image.className = "adventurer_image";
+    image.alt = adventurer_css_id;
+    image.width = "55";
+    image.height = "55";
+    const image_source = "static/images/" + event.target.adventurer_class.value + ".jpeg";
+    image.src = image_source;
+    adventurer_class_details.appendChild(image);
 
     const contract = document.createElement('p');
-    contract.textContent = event.target.contract.value;
+    contract.textContent = `Initiation Contract: ${event.target.contract.value}`;
     newAdventurer.appendChild(contract);
     
     event.target.reset();
+}
+
+const handleDeleteAllClick = function (event) {
+    event.preventDefault();
+
+    const adventurer_roster = document.querySelector('#adventurer-roster');
+    adventurer_roster.innerHTML = '';
 }
